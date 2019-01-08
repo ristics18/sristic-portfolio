502a4b2d9839ec59000417bee0ca7aa9ab6b9904
Srdan Ristic <srdjanristic11@gmail.com>
2019-01-07 21:03:24 -0600

    Worked on the content

--- main/src/constants/app.constants.ts
+++ main/src/constants/app.constants.ts
@@ -8,14 +8,14 @@ export class AppConstants {
   readonly MY_LOGO = "logo.png";
   readonly MY_IMAGE = "profile.png";
   readonly MY_TITLE = "Full Stack Developer";
-  readonly MY_SUMMARY = "Profile info goes here. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet.";
+  readonly MY_SUMMARY = "Hi! Thank you for taking your time to check out my website! Let me introduce myself. I am a dedicated, focused, organized, hardworking and dependable individual who excels at learning new technologies and techniques, working individually or in a team setting, capable of completing multiple tasks simultaneously with high-quality results. I enjoy being involved in both front-end and back-end within the scope of a full software development cycle. I have built various cross-platform projects and enjoyed working on every single one of them. I am currently working for PioneerRx Pharmacy Software in Shreveport, LA.";
   readonly MY_EMAIL = "srdanristic2@gmail.com";
   readonly MY_PHONE = "318-900-1294";
   readonly MY_CITY = "Shreveport, LA";
   readonly MY_SKYPE = "srlepn";
   readonly MY_WEBSITE_URL = "https://www.sristic.com";
-  readonly MY_SKILLS_SUMMARY = "I have more than 8 years' experience building rich web applications for clients all over the world. Below is a quick overview of my ma. Want to find out more about my experience?";
-  readonly MY_WHATS_NEW = "Here you can use rows and columns here to organize your footer content.asldjasd asdasdasdasdasdas asdha sldaks lkashdlasdkjash";
+  readonly MY_SKILLS_SUMMARY = "I have 3 years of experience building mobile,web and desktop applications. Below is a quick overview of my skills and certifications, feel free to explore. Want to find out more about my experience?";
+  readonly MY_WHATS_NEW = "Besides my full-time job I am also tutoring computer science, participating in open source projects and doing some freelancing.";
   readonly MY_RESUME = "Srdan Ristic Resume.pdf";
 
   // Links
@@ -28,7 +28,7 @@ export class AppConstants {
   readonly MY_IMAGE_AM = "about-me.png";
   readonly MY_TITLE_AM = "It's all about happiness";
   readonly MY_SUBTITLE_AM = "Treat people the way you want to be treated.";
-  readonly MY_SUMMARY_AM = "Profile info goes here. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet.";
+  readonly MY_SUMMARY_AM = "Let me start of with a simple introduction. My name is Srđan Ristic and I come from Serbia, a small country in Southeastern Europe. In United States I go by Surgen. My life is focused on happiness just like I stated above. If I am not happy I make a change. I always think of it as if today was the last day on Earth, the question would be: Am I going to start complaining about the things that I don't like or don't want to do? No, the idea is to live this life at it's fullest and be positive. There are two things that make me the happiest person on earth and those are basketball and development. I am all about sports and socializing, always interested in meeting new people and making connections.";
 
   // Experience
   readonly EXPERIENCE = [
@@ -36,41 +36,42 @@ export class AppConstants {
       experienceName: "Full Stack Developer",
       experienceCompany: "PioneerRx Pharmacy Software",
       experienceDate: "18 Jun 2018 - Current",
-      experienceDesc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient."
+      experienceDesc: "This is my current job where I develop and maintain software for pharmacies (patients, prescriptions, prescribers etc.). My involvement is mainly focused on centralized part of the pharmacies and their stores. I am also working on some the the internal web projects."
     },
     {
       experienceName: "Graduate Research Assistant",
       experienceCompany: "Louisiana State University Shreveport",
       experienceDate: "31 Jan 2017 - 01 May 2018",
-      experienceDesc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient."
+      experienceDesc: "Worked on a variety of biomedical informatics projects. With the use of powerful computers, I learned and worked on managing and analyzing biological, chemical, and medical processes in the interest of improving medicine and patient care."
     },
     {
       experienceName: "Intern/Junior Software Developer",
       experienceCompany: "Benetech Corporation",
       experienceDate: "01 Oct 2015 - 01 May 2016",
-      experienceDesc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient."
+      experienceDesc: "Besides learning and acquiring new skills using various programming technologies, I worked on development and maintenance of the systems for content management, using technologies such as Java, Spring, REST, SQL, Alfresco, CMIS and AngularJS. I communicated with clients on a daily bases."
     }
   ];
 
   // Education
   readonly EDUCATION = [
     {
-      educationName: "Master of Computer Systems Technology",
+      educationName: "MS in Computer Systems Technology",
       educationUniversity: "Louisiana State University Shreveport",
       educationDate: "01 Jan 2017 - 15 May 2018",
-      educationDesc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient"
+      educationDesc: "Attended LSUS for little bit more than a year in order to complete my Master's. Worked on variety of projects in which my favorite one was the capstone project (Android app)."
     },
+
     {
-      educationName: "Bachelor of Information Technology",
+      educationName: "BS in Information Technology",
       educationUniversity: "University Metropolitan",
       educationDate: "01 Oct 2012 - 01 Dec 2016",
-      educationDesc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient"
+      educationDesc: "Pursued my Bachelor's degree in Belgrade, Serbia. That was my starting point when it comes to software development. Mainly focused on Java projects."
     },
     {
       educationName: "High School",
       educationUniversity: "Sports High School in Belgrade",
       educationDate: "01 Sep 2008 - 12 Apr 2012",
-      educationDesc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient"
+      educationDesc: "Attended Sports High School in Belgrade where the main focus besides school was basketball. Spent my junior year in Raymond, IL."
     }
   ];
 
@@ -87,7 +88,7 @@ export class AppConstants {
           skillName: "Bootstrap 3/4"
         },
         {
-          skillName: "JS/AngularJS/Angular 7"
+          skillName: "JS/AngularJS/Angular"
         },
         {
           skillName: "NodeJS"
@@ -123,7 +124,7 @@ export class AppConstants {
       skillImage: "other.svg",
       skills: [
         {
-          skillName: "Git/Subversion"
+          skillName: "Git/TFS/Subversion"
         },
         {
           skillName: "Maven/Gradle"
@@ -149,7 +150,7 @@ export class AppConstants {
       caaImage: "microsoft-cert.png",
       caaDate: "In Progress",
       caaLink: "#",
-      caaDesc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient"
+      caaDesc: "I am currently pursuing this certification, which is consisted of two exams 70-701 and 70-702. I have already passed the 701 and am currently studying for 702."
     },
     {
       caaName: "Oracle Certified Associate, Java SE 8 Programmer",
@@ -157,7 +158,7 @@ export class AppConstants {
       caaImage: "oracle-cert.png",
       caaDate: "14 Nov 2017",
       caaLink: "https://www.youracclaim.com/badges/b0610484-16e7-43c4-af31-851ebfc2c002/linked_in_profile",
-      caaDesc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient"
+      caaDesc: "I am a big fan of Java, one of the reasons why I pursued and successfully completed this certification. My future goal is to take Oracle Certified Professional exam."
     },
     {
       caaName: "University Award for Outstanding Student",
@@ -165,7 +166,7 @@ export class AppConstants {
       caaImage: "lsus-award.png",
       caaDate: "03 Apr 2018",
       caaLink: "https://www.lsus.edu/news-and-events/lsus-honors-its-best-of-the-best-at-academic-awards-convocation-2018",
-      caaDesc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient"
+      caaDesc: "I was chosen as an outstanding student at the end of my Master's at LSUS. This was a nice surprise for me that showed me that hard work pays off."
     }
   ];
 
