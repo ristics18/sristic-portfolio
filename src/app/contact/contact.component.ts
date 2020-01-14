22ac5227662fd895f0e5f673ccd1557c1ea0d61f
Srdan Ristic <srdanristic2@gmail.com>
2020-01-14 22:37:38 +0100

    Updated Slack web hook

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -33,7 +33,7 @@ export class ContactComponent implements OnInit {
       return;
     }
 
-    this.http.postRequest('https://hooks.slack.com/services/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', {
+    this.http.postRequest('https://hooks.slack.com/services/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', {
       "text":
         "Name: " + this.messageForm.get('name').value +
         "\n Email: " +  this.messageForm.get('email').value +
